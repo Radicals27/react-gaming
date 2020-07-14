b1209a7056a3adabf9847164a0de746a295774ff
Radicals <cclose27@gmail.com>
2020-07-15 09:05:25 +1000

    Google login working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react"
+import GoogleLogin from "react-google-login"
 
-function App() {
+export default function App() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [url, setUrl] = useState("")
+
+  const responseGoogle = response => {
+    setName(response.profileObj.name)
+    setEmail(response.profileObj.email)
+    setUrl(response.profileObj.imageUrl)
+  }
+
   return (
-    <div>
+    <div className="App">
+      <h1>Login with Google:</h1>
+      <h2>Welcome: {name}</h2>
+      <h2>Email: {email}</h2>
+      <img src={url} alt={name} />
+      <GoogleLogin
+        clientId="157895230249-agrvo2b25jecn7isgs81hsv77khm7lfs.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
-  );
+  )
 }
-
-export default App;
