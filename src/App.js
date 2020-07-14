dd594755dbeb21013ba356137aa09d57fdeb3a73
Radicals <cclose27@gmail.com>
2020-07-15 09:51:24 +1000

    asks user to login at home page, user can login now with Google.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import GoogleLogin from "react-google-login"
 
 export default function App() {
@@ -13,19 +13,44 @@ export default function App() {
     setUrl(response.profileObj.imageUrl)
   }
 
+  function UserGreeting() {
+    return ( <>
+        <h1>Welcome back {name}!</h1>
+        <h2>Welcome: {name}</h2>
+        <h2>Email: {email}</h2>
+        <img src={url} alt={name} />
+      </>
+    )
+  }
+
+  function GuestGreeting() {
+    return (
+      <>
+        <h1>Welcome!</h1>
+        <h2>Please Log in:</h2>
+        <GoogleLogin
+          clientId="157895230249-agrvo2b25jecn7isgs81hsv77khm7lfs.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </>
+    )
+  }
+
+  function LoginCheck() {
+    if (email != "") {
+      return <UserGreeting />
+    }
+    return <GuestGreeting />
+  }
+
   return (
     <div className="App">
-      <h1>Login with Google:</h1>
-      <h2>Welcome: {name}</h2>
-      <h2>Email: {email}</h2>
-      <img src={url} alt={name} />
-      <GoogleLogin
-        clientId="157895230249-agrvo2b25jecn7isgs81hsv77khm7lfs.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-      />     
+
+    <LoginCheck/>
+      
     </div>
   )
 }
