ed7edeeea98fa5c53a5257e82d12e8d92c7e5475
BrieferCelery <aa.kane1994@gmail.com>
2020-07-15 15:18:39 +1000

    home page first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Search from './Search'
+import HomePage from './HomePage'
 
 function App() {
   return (
     <div>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+
+export default function Search() {
+
+
+    return (
+        <div>
+            <ul>
+
+            </ul>          
+        </div>
+    )
+}
