3ae9508358cc0379ff1f347aa883ce5b8d4af9d6
Radicals <cclose27@gmail.com>
2020-07-15 14:49:18 +1000

    Revert "testing"
    This reverts commit 32c019db60eec15f3dd3f460c7060ec663be900e.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import React, { useState } from "react"
 import ReactDOM from 'react-dom'
+import GoogleLogin from "react-google-login"
 
 export default function App() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [url, setUrl] = useState("")
+
+  const responseGoogle = response => {
+    setName(response.profileObj.name)
+    setEmail(response.profileObj.email)
+    setUrl(response.profileObj.imageUrl)
+  }
+
+  function UserGreeting() {
+    return ( <>
+        <h1>Welcome back {name}!</h1>
+        <h2>Welcome: {name}</h2>
+        <h2>Email: {email}</h2>
+        <img src={url} alt={name} />
+      </>
+    )
+  }
+
+  function GuestGreeting() {
+    return (
+      <>
+        <h1>Welcome!</h1>
+        <h2>Please Log in:</h2>
+        <GoogleLogin
+          clientId="157895230249-agrvo2b25jecn7isgs81hsv77khm7lfs.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </>
+    )
+  }
+
+  function LoginCheck() {
+    if (email != "") {
+      return <UserGreeting />
+    }
+    return <GuestGreeting />
+  }
+
   return (
-    <h1> HELLO </h1>
+    <div className="App">
+
+    <LoginCheck/>
+      
+    </div>
   )
 }
-
-
 
