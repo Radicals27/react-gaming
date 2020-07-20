ab459bd0d0606288b9924650a69966ac1dba2580
Radicals <cclose27@gmail.com>
2020-07-21 09:22:54 +1000

    conrads ping test working

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom'
 import './styles/App.css'
 import GoogleLogin from "react-google-login"
 import HomePage from './HomePage'
-
 import Search from './Search'
+import Axios from "axios"
 
 export default function App() {
+    const handleClick = (event) => {
+        event.preventDefault()
+        Axios.get('http://localhost:4000/ping') 
+            .then(res => console.log(res))
+
+    }
+
     return (
         <div class="App-header">
+            <button onClick={handleClick}>
+                Click Me
+            </button>
             <HomePage />
         </div>
     )
