261c1ef05c3b3f2af4b328c55d3bd850557d40aa
Aaron Kane <aaronkane@192-168-1-128.tpgi.com.au>
2020-07-22 18:37:07 +1000

    added cookie bar and tidied app section

---
+++ src/CookieBar.jsx
@@ -0,0 +1,35 @@
+import React, { useState, Fragment } from 'react'
+import MUICookieConsent from 'material-ui-cookie-consent';
+
+
+export default function CookieBar() {
+  const [isOpened, setIsOpened] = useState(true)
+
+  const closeIt = () => {
+    setIsOpened(false)
+  }
+
+  return (
+    // <Fragment>
+    //   {isOpened ? (
+    //     <section id='cookieBar' className='bg-dark text-light sticky-top py-2'>
+    //       <div className='container'>
+    //         This page doesn't store cookies, but RAWG does. Check out{' '}
+    //         <a className='text-warning' href='https://rawg.io/privacy_policy'>
+    //           their cookie policy
+    //         </a>{' '}
+    //         to opt out!
+    //         <button onClick={closeIt} type='button' className='close text-light'>
+    //           <span>&times;</span>
+    //         </button>
+    //       </div>
+    //     </section>
+    //   ) : null}
+    // </Fragment>
+    <MUICookieConsent 
+        cookieName="mySiteCookieConsent"
+        message="We don't store cookies but RAWG does please visit 
+        their website for more information :)"
+    />
+  )
+}
