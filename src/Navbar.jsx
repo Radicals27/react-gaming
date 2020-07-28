4814ac03cfb283eb3ff16b18d59aeec0ea1f1047
Radicals <cclose27@gmail.com>
2020-07-28 14:38:58 +1000

    removed login from navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+
 import PropTypes from 'prop-types'
 import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Slide,
-  Button,
-  CssBaseline,
-  useScrollTrigger,
-  IconButton
+    AppBar,
+    Toolbar,
+    Typography,
+    Slide,
+    Button,
+    CssBaseline,
+    useScrollTrigger,
+    IconButton
 } from '@material-ui/core'
 // import WhatshotIcon from '@material-ui/icons'
 import Search from './Search'
@@ -16,40 +17,40 @@ import './styles/Navbar.css'
 import Auth from './Auth'
 
 function HideOnScroll(props) {
-  const { children, window } = props
-  const trigger = useScrollTrigger({ target: window ? window() : undefined })
+    const { children, window } = props
+    const trigger = useScrollTrigger({ target: window ? window() : undefined })
 
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  )
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    )
 }
 
 export default function Navbar(props) {
 
-  return (
-    <>
-      {/* <CssBaseline /> */}
-        <HideOnScroll {...props}>
-          <AppBar colour="secondary">
-            <Toolbar>
-              <Typography variant="h6">REACT GAMING</Typography>
-              <Button>
-                Home
-              </Button>
-              <Button onClick={() => console.log("Our Mission clicked")}>
-                Our Mission
-                </Button>
-              <Search/>
-              <Auth>
-              </Auth>
-            </Toolbar>
+    return (
+        <>
+            {/* <CssBaseline /> */}
+            <HideOnScroll {...props}>
+                <AppBar colour="secondary">
+                    <Toolbar>
+                        <Typography variant="h6">REACT GAMING</Typography>
+                        <Button>
+                            Home
+                        </Button>
+                        <Button>
+                            Our Mission
+                        </Button>
+                        <Search />
+                        <Auth>
+                        </Auth>
+                    </Toolbar>
 
-          </AppBar>
-        </HideOnScroll>
-      {/* <Toolbar /> */}
+                </AppBar>
+            </HideOnScroll>
+            {/* <Toolbar /> */}
 
-    </>
-  )
+        </>
+    )
 }
