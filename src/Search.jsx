418e5544bc7569a733b1ced72d64d822ab27b063
Radicals <cclose27@gmail.com>
2020-07-29 10:03:35 +1000

    moved logged in as to the right of navbar

--- src/Search.jsx
+++ src/Search.jsx
@@ -39,7 +39,7 @@ useEffect(() => {
     return (
         <>
             <div className="search" style={ {zIndex: 1}}>
-                <Input onChange={setKeywordInInput} value={keyword} placeholder={"Chicken"}/>
+                <Input onChange={setKeywordInInput} value={keyword} placeholder={"Searcch for game"}/>
                 {dataIsReady ? (
                 <Fragment>
                     {dropdownIsopened ? (
